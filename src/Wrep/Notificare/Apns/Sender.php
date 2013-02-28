ddd8b3f0c09584d301af97c99383ddbba21df53b
Mathijs Kadijk <mathijs@wrep.nl>
2013-02-28 17:55:57 +0100

    Sender finished

--- src/Wrep/Notificare/Apns/Sender.php
+++ src/Wrep/Notificare/Apns/Sender.php
@@ -7,22 +7,44 @@ class Sender
 	private $defaultCertificate;
 	private $connectionPool;
 
+	/**
+	 * Construct Sender
+	 *
+	 * @param $defaultCertificate Certificate|null The certificate to use when no certificate is given in the message itself
+	 */
 	public function __construct(Certificate $defaultCertificate = null)
 	{
 		$this->setDefaultCertificate($defaultCertificate);
 		$this->connectionPool = array();
 	}
 
+	/**
+	 * Set a default certificate
+	 *
+	 * @param $defaultCertificate Certificate|null The certificate to use when no certificate is given in the message itself
+	 */
 	public function setDefaultCertificate(Certificate $defaultCertificate)
 	{
 		$this->defaultCertificate = $defaultCertificate;
 	}
 
+	/**
+	 * Get the current default certificate
+	 *
+	 * @return Certificate|null
+	 */
 	public function getDefaultCertificate()
 	{
 		return $this->defaultCertificate;
 	}
 
+	/**
+	 * Queue a message on the correct APNS connection
+	 * Note: A certificate must be set in the message or as default to make this method work
+	 *
+	 * @param $message Message The message to queue
+	 * @return MessageEnvelope
+	 */
 	public function queue(Message $message)
 	{
 		// Get specific certificate from the message
@@ -44,14 +66,40 @@ class Sender
 
 		// Get the connection for the certificate
 		$connection = $this->getConnectionForCertificate($certificate);
-		// TODO; Queue on the connection
+
+		// Queue the message
+		return $connection->queue($message);
 	}
 
-	public function flush()
+	/**
+	 * Send all queued messages
+	 *
+	 * @param $certificate Certificate|null When given only the connection for the given certificate is flushed
+	 */
+	public function flush(Certificate $certificate = null)
 	{
-
+		// Check if we must flush a specific connection
+		if (null == $certificate)
+		{
+			// No flush the whole connection pool
+			foreach ($this->connectionPool as $connection)
+			{
+				$connection->flush();
+			}
+		}
+		else
+		{
+			// Yes, flush only the requested connection
+			$this->getConnectionForCertificate($certificate)->flush();
+		}
 	}
 
+	/**
+	 * Get/create the connection associated with the given certificate
+	 *
+	 * @param $certificate Certificate The certificate to get the connection for
+	 * @return Connection
+	 */
 	private function getConnectionForCertificate(Certificate $certificate)
 	{
 		// If no connection is available for this certificate create one
