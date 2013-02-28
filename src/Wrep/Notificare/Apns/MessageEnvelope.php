4f86cdff42eba0363cb887c31180aeda412a9b1b
Mathijs Kadijk <mathijs@wrep.nl>
2013-02-28 14:51:02 +0100

    Dummy sender and error status in envelope

--- src/Wrep/Notificare/Apns/MessageEnvelope.php
+++ src/Wrep/Notificare/Apns/MessageEnvelope.php
@@ -4,12 +4,34 @@ namespace Wrep\Notificare\Apns;
 
 class MessageEnvelope
 {
+	const STATUS_NOERRORS = 0;
+	const STATUS_SENDFAILED = 256;
+	const STATUS_EARLIERERROR = 257;
+
 	const BINARY_COMMAND = 1;
 	const BINARY_DEVICETOKEN_SIZE = 32;
 
 	private $identifier;
 	private $message;
-	private $errors;
+	private $status;
+
+	private static $statusDescriptionMapping = array(
+			 -1 => 'Not send to APNS',
+
+			  0 => '[APNS] No errors encountered',
+			  1 => '[APNS] Processing error',
+			  2 => '[APNS] Missing device token',
+			  3 => '[APNS] Missing topic',
+			  4 => '[APNS] Missing payload',
+			  5 => '[APNS] Invalid token size',
+			  6 => '[APNS] Invalid topic size',
+			  7 => '[APNS] Invalid payload size',
+			  8 => '[APNS] Invalid token',
+			255 => '[APNS] Unknown error',
+
+			256 => 'Sending failed, will retry with other envelope',
+			257 => 'Failed due earlier error, will retry with other envelope',
+		);
 
 	public function __construct($identifier, Message $message)
 	{
@@ -26,6 +48,7 @@ class MessageEnvelope
 		// Save the given parameters
 		$this->identifier = $identifier;
 		$this->message = $message;
+		$this->status = -1;
 	}
 
 	public function getIdentifier()
@@ -36,6 +59,39 @@ class MessageEnvelope
 	public function getMessage()
 	{
 		return $this->message;
+	}
+
+	public function setStatus($status)
+	{
+		// Ignore if status stays the same
+		if ($this->getStatus() == $status) {
+			return;
+		}
+
+		// Check if we're not in a final state yet
+		if ($this->status > 0) {
+			throw new \RuntimeException('Cannot change status from final state ' . $this->status . ' to state ' . $status . '.');
+		}
+
+		// Check if this is a valid state
+		if ( !in_array($status, array_keys(self::$statusDescriptionMapping)) ) {
+			throw new \InvalidArgumentException('Status ' . $status . ' is not a valid status.');
+		}
+
+		// Save it!
+		$this->status = $status;
+
+		echo 'MessageEnvelope #' . $this->getIdentifier() . ' is marked as ' . $this->getStatusDescription() . PHP_EOL;
+	}
+
+	public function getStatus()
+	{
+		return $this->getStatus;
+	}
+
+	public function getStatusDescription()
+	{
+		return self::$statusDescriptionMapping[$this->getStatus];
 	}
 
 	public function getBinaryMessage()
---
+++ src/Wrep/Notificare/Apns/Sender.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Wrep\Notificare\Apns;
+
+class Sender
+{
+	public function queue(Message $message)
+	{
+		//return MessageEnvelope;
+	}
+
+	public function flush()
+	{
+
+	}
+}
