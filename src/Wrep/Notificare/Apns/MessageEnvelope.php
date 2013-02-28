d7224b14a0c1eb86e655b40ad3a3bbf088324685
Mathijs Kadijk <mathijs@wrep.nl>
2013-02-28 15:40:20 +0100

    Message envelope comments

--- src/Wrep/Notificare/Apns/MessageEnvelope.php
+++ src/Wrep/Notificare/Apns/MessageEnvelope.php
@@ -4,17 +4,21 @@ namespace Wrep\Notificare\Apns;
 
 class MessageEnvelope
 {
+	// Statuscode constants
 	const STATUS_NOERRORS = 0;
 	const STATUS_SENDFAILED = 256;
 	const STATUS_EARLIERERROR = 257;
 
+	// Binary message constants
 	const BINARY_COMMAND = 1;
 	const BINARY_DEVICETOKEN_SIZE = 32;
 
+	// Attributes
 	private $identifier;
 	private $message;
 	private $status;
 
+	// Get human readable strings for the statuscodes
 	private static $statusDescriptionMapping = array(
 			 -1 => 'Not send to APNS',
 
@@ -33,6 +37,12 @@ class MessageEnvelope
 			257 => 'Failed due earlier error, will retry with other envelope',
 		);
 
+	/**
+	 * Construct MessageEnvelope
+	 *
+	 * @param $identifier int Unique number to the relevant APNS connection to identify this message
+	 * @param $message Message The message that's is contained by this envelope
+	 */
 	public function __construct($identifier, Message $message)
 	{
 		// A message id greater then 0 is required
@@ -51,16 +61,32 @@ class MessageEnvelope
 		$this->status = -1;
 	}
 
+	/**
+	 * Unique number to the relevant APNS connection to identify this message
+	 *
+	 * @return int
+	 */
 	public function getIdentifier()
 	{
 		return $this->identifier;
 	}
 
+	/**
+	 * The message that's is contained by this envelope
+	 *
+	 * @return Message
+	 */
 	public function getMessage()
 	{
 		return $this->message;
 	}
 
+	/**
+	 * Set the status of this message envelope
+	 *  only possible if there is no final state set yet.
+	 *
+	 * @param $status int One of the keys in self::$statusDescriptionMapping
+	 */
 	public function setStatus($status)
 	{
 		// Ignore if status stays the same
@@ -82,16 +108,31 @@ class MessageEnvelope
 		$this->status = $status;
 	}
 
+	/**
+	 * Get the current status of this message envelope
+	 *
+	 * @return int
+	 */
 	public function getStatus()
 	{
 		return $this->status;
 	}
 
+	/**
+	 * Get a description of the current status of this message envelope
+	 *
+	 * @return string
+	 */
 	public function getStatusDescription()
 	{
 		return self::$statusDescriptionMapping[$this->getStatus()];
 	}
 
+	/**
+	 * Get the message that this envelope contains in binary APNS compatible format
+	 *
+	 * @return string
+	 */
 	public function getBinaryMessage()
 	{
 		$jsonMessage = $this->getMessage()->getJson();
