32b0fcb24bf71f853de5625b2f8961e69a5afbf7
Mathijs Kadijk <mathijs@wrep.nl>
2013-03-25 23:54:28 +0100

    Moved private setters down and added PHP doc to them

--- src/Wrep/Notificato/Apns/Message.php
+++ src/Wrep/Notificato/Apns/Message.php
@@ -54,105 +54,6 @@ class Message
 		}
 	}
 
-	private function setDeviceToken($deviceToken)
-	{
-		// Check if a devicetoken is given
-		if (null == $deviceToken) {
-			throw new \InvalidArgumentException('No device token given.');
-		}
-
-		// Check if the devicetoken is a valid hexadecimal string
-		if (!ctype_xdigit($deviceToken)) {
-			throw new \InvalidArgumentException('Invalid device token given, no hexadecimal: ' . $deviceToken);
-		}
-
-		// Check if the length of the devicetoken is correct
-		if (64 != strlen($deviceToken)) {
-			throw new \InvalidArgumentException('Invalid device token given, incorrect length: ' . $deviceToken . ' (' . strlen($deviceToken) . ')');
-		}
-
-		// Set the devicetoken
-		$this->deviceToken = $deviceToken;
-	}
-
-	private function setAlert($alert)
-	{
-		if (null == $alert)
-		{
-			// No alert is okay
-			$this->alert = null;
-		}
-		else if ( is_string($alert) )
-		{
-			// String only alert is okay
-			$this->alert = $alert;
-		}
-		else if ( is_array($alert) )
-		{
-			if ( isset($alert['body']) || (isset($alert['loc-key']) && isset($alert['loc-args'])) )
-			{
-				// Valid alert, just fine!
-				$this->alert = $alert;
-			}
-			else
-			{
-				// Alert must contain correct keys
-				throw new \InvalidArgumentException('Invalid alert for message. Alert does not contain a body key or the loc-key and loc-args keys.');
-			}
-		}
-		else
-		{
-			// Alert must be null or an array
-			throw new \InvalidArgumentException('Invalid alert for message. Alert was not an array or null value.');
-		}
-	}
-
-	private function setBadge($badge)
-	{
-		// Validate the badge int
-		if ((int)$badge < 0) {
-			throw new \InvalidArgumentException('Badge must be 0 or higher.');
-		}
-
-		// Cast to int or set to null
-		$this->badge = (null === $badge) ? null : (int)$badge;
-	}
-
-	private function setPayload($payload)
-	{
-		if ( (is_string($payload) && empty($payload)) || (is_array($payload) && count($payload) == 0) )
-		{
-			// Empty strings or arrays are not allowed
-			throw new \InvalidArgumentException('Invalid payload for message. Payload was empty, but not null)');
-		}
-		else if (is_array($payload) || null === $payload)
-		{
-			if ( isset($payload['aps']) )
-			{
-				// Reserved key is used
-				throw new \InvalidArgumentException('Invalid payload for message. Custom payload may not contain the reserved "aps" key.');
-			}
-			else
-			{
-				// This is okay, set as payload
-				$this->payload = $payload;
-			}
-		}
-		else
-		{
-			// Try to decode JSON string payload
-			$payload = json_decode($payload, true);
-
-			// Check if decoding the payload worked
-			if (null === $payload) {
-				throw new \InvalidArgumentException('Invalid payload for message. Payload was invalid JSON.');
-			}
-
-			// Set as payload
-			$this->payload = $payload;
-		}
-	}
-
 	/**
 	 * Get the device token of the receiving device
 	 *
@@ -282,5 +183,129 @@ class Message
 		}
 
 		return $json;
+	}
+
+		/**
+	 * Set the receiver of the message
+	 *
+	 * @param string Receiver of this message
+	 * @throws \InvalidArgumentException On invalid or missing arguments
+	 */
+	private function setDeviceToken($deviceToken)
+	{
+		// Check if a devicetoken is given
+		if (null == $deviceToken) {
+			throw new \InvalidArgumentException('No device token given.');
+		}
+
+		// Check if the devicetoken is a valid hexadecimal string
+		if (!ctype_xdigit($deviceToken)) {
+			throw new \InvalidArgumentException('Invalid device token given, no hexadecimal: ' . $deviceToken);
+		}
+
+		// Check if the length of the devicetoken is correct
+		if (64 != strlen($deviceToken)) {
+			throw new \InvalidArgumentException('Invalid device token given, incorrect length: ' . $deviceToken . ' (' . strlen($deviceToken) . ')');
+		}
+
+		// Set the devicetoken
+		$this->deviceToken = $deviceToken;
+	}
+
+	/**
+	 * Set the alert to display.
+	 *  See also: http://developer.apple.com/library/ios/#documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/ApplePushService/ApplePushService.html#//apple_ref/doc/uid/TP40008194-CH100-SW1
+	 *
+	 * @param array|null The alert to display or null to set no alert
+	 * @throws \InvalidArgumentException On invalid or missing arguments
+	 */
+	private function setAlert($alert)
+	{
+		if (null == $alert)
+		{
+			// No alert is okay
+			$this->alert = null;
+		}
+		else if ( is_string($alert) )
+		{
+			// String only alert is okay
+			$this->alert = $alert;
+		}
+		else if ( is_array($alert) )
+		{
+			if ( isset($alert['body']) || (isset($alert['loc-key']) && isset($alert['loc-args'])) )
+			{
+				// Valid alert, just fine!
+				$this->alert = $alert;
+			}
+			else
+			{
+				// Alert must contain correct keys
+				throw new \InvalidArgumentException('Invalid alert for message. Alert does not contain a body key or the loc-key and loc-args keys.');
+			}
+		}
+		else
+		{
+			// Alert must be null or an array
+			throw new \InvalidArgumentException('Invalid alert for message. Alert was not an array or null value.');
+		}
+	}
+
+	/**
+	 * Set the badge to display on the App icon
+	 *
+	 * @param int|null The badge number to display, zero to remove badge
+	 * @throws \InvalidArgumentException On invalid or missing arguments
+	 */
+	private function setBadge($badge)
+	{
+		// Validate the badge int
+		if ((int)$badge < 0) {
+			throw new \InvalidArgumentException('Badge must be 0 or higher.');
+		}
+
+		// Cast to int or set to null
+		$this->badge = (null === $badge) ? null : (int)$badge;
+	}
+
+	/**
+	 * Set custom payload to go with the message
+	 *
+	 * @param array|json|null The payload to send as array or JSON string
+	 * @throws \InvalidArgumentException On invalid or missing arguments
+	 */
+	private function setPayload($payload)
+	{
+		if ( (is_string($payload) && empty($payload)) || (is_array($payload) && count($payload) == 0) )
+		{
+			// Empty strings or arrays are not allowed
+			throw new \InvalidArgumentException('Invalid payload for message. Payload was empty, but not null)');
+		}
+		else if (is_array($payload) || null === $payload)
+		{
+			if ( isset($payload['aps']) )
+			{
+				// Reserved key is used
+				throw new \InvalidArgumentException('Invalid payload for message. Custom payload may not contain the reserved "aps" key.');
+			}
+			else
+			{
+				// This is okay, set as payload
+				$this->payload = $payload;
+			}
+		}
+		else
+		{
+			// Try to decode JSON string payload
+			$payload = json_decode($payload, true);
+
+			// Check if decoding the payload worked
+			if (null === $payload) {
+				throw new \InvalidArgumentException('Invalid payload for message. Payload was invalid JSON.');
+			}
+
+			// Set as payload
+			$this->payload = $payload;
+		}
 	}
 }
